45b7c011a95cc0ba586fd360f351d69b85635f13
gojaebeom <const.gjb@gmail.com>
2020-10-25 19:39:55 +0900

    done

---
+++ pages/signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
+
+const Signup = () => {
+    return (
+    <>
+        <Head>
+            <title>NodeBird 회원가입</title>
+        </Head>
+        <AppLayout>회원가입!</AppLayout>
+    </>
+    )
+};
+
+export default Signup;
